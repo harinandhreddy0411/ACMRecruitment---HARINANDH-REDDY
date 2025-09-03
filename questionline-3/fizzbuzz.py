39533ef74aa0b47c976a85ec6ed35cdcc5573489
harinandhreddy0411 <harinandhreddy@gmail.com>
2025-09-03 12:28:10 +0530

    Update fizzbuzz.py

--- questionline-3/fizzbuzz.py
+++ questionline-3/fizzbuzz.py
@@ -7,3 +7,6 @@ for i in range(1, 51):
         print("Buzz")
     else:
         print(i)
+
+![image alt](https://github.com/harinandhreddy0411/ACMRecruitment---HARINANDH-REDDY/blob/ee66cf7a66c7dd03fc4de77d2b066098b0799d6f/questionline-3/Screenshot%202025-09-03%20122501.png)
+
