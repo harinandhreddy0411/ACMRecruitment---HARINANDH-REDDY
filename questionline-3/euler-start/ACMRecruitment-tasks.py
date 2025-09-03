b4f1ee2f284c0d21cb2dc5c1cf33b3a3c84b4f30
harinandhreddy0411 <harinandhreddy@gmail.com>
2025-09-03 13:13:24 +0530

    Add files via upload

---
+++ questionline-3/euler-start/ACMRecruitment-tasks.py
@@ -0,0 +1,12 @@
+n = 100
+sum_n = n * (n + 1) // 2
+square_of_sum = sum_n ** 2
+sum_of_squares = n * (n + 1) * (2 * n + 1) // 6
+difference = square_of_sum - sum_of_squares
+print("Difference:", difference)
+
+total = 0
+for i in range(1000):
+    if i % 3 == 0 or i % 5 == 0:
+        total += i
+print("Sum:", total)
